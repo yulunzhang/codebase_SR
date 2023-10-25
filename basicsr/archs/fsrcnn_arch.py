60ddbc6cfdc065d17842aa0aa5effa22edfe89d3
zhengchen1999 <463465810@qq.com>
2023-10-25 13:28:19 +0000

    initial

--- basicsr/archs/fsrcnn_arch.py
+++ basicsr/archs/fsrcnn_arch.py
@@ -1,33 +1,8 @@
+import torch
 import torch.nn as nn
-import math
-
 from basicsr.utils.registry import ARCH_REGISTRY
 
 @ARCH_REGISTRY.register()
-class FSRCNN(nn.Module):
-    def __init__(self, scale_factor, num_channels=1, d=56, s=12, m=4):
-        super(FSRCNN, self).__init__()
-        self.first_part = nn.Sequential(
-            nn.Conv2d(num_channels, d, kernel_size=5, padding=2),
-            nn.PReLU(d)
-        )
-        self.mid_part = [nn.Conv2d(d, s, kernel_size=1), nn.PReLU(s)]
-        for _ in range(m):
-            self.mid_part.extend([nn.Conv2d(s, s, kernel_size=3, padding=3//2), nn.PReLU(s)])
-        self.mid_part.extend([nn.Conv2d(s, d, kernel_size=1), nn.PReLU(d)])
-        self.mid_part = nn.Sequential(*self.mid_part)
-        self.last_part = nn.ConvTranspose2d(d, num_channels, kernel_size=9, stride=scale_factor, padding=9//2,
-                                            output_padding=scale_factor-1)
-
-    def forward(self, x):
-        x = self.first_part(x)
-        x = self.mid_part(x)
-        x = self.last_part(x)
-        return x
-
-import torch
-import torch.nn as nn
-
 class FSRCNN(nn.Module):
     def __init__(self, upscale_factor, num_channels=1, d=56, s=12, m=4):
         super(FSRCNN, self).__init__()
@@ -69,8 +44,18 @@ class FSRCNN(nn.Module):
     def forward(self, x):
         out = self.feature_extraction(x)
         out = self.shrinking(out)
-        out = self.mapping(out)
-        out = self.expanding(out)
+        out1 = self.mapping(out)
+        out = self.expanding(out1)
         out = self.deconvolution(out)
         return out
 
+if __name__ == "__main__":
+    upscale = 3
+    height = 80
+    width = 71
+    model = FSRCNN(upscale_factor=upscale, num_channels=3, m=2)
+    x = torch.randn((1, 3, height, width))
+    x = model(x)
+    print(x.shape)
+
+    
